14204009e4b34a7177ab63f16d961cf24e2f1f69
Arthur-Byamukama <48201055+Arthur-Byamukama@users.noreply.github.com>
2019-07-22 12:11:23 +0300

    1st commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+$("img").click(function() {
+  alert("This is an image.");
+});
